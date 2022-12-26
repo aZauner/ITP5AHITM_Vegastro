9b3306e97b5a9514633ab5782dcd77394c8e4c0e
Alexander Zauner <alexanderzauner@MacBook-Pro-von-Alexander.local>
2022-12-26 23:09:25 +0100

    Made Searchbar functioning added Map User Location

--- Code/vegastro/src/app/tab1/tab1.page.ts
+++ Code/vegastro/src/app/tab1/tab1.page.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
 import * as L from 'leaflet';
-import { Geolocation } from '@awesome-cordova-plugins/geolocation/ngx';
+import { Geolocation, Geoposition } from '@awesome-cordova-plugins/geolocation/ngx';
 import { Platform } from '@ionic/angular';
+import { Coordinates } from '@awesome-cordova-plugins/geolocation';
+import { AbstractFormGroupDirective } from '@angular/forms';
 
 @Component({
   selector: 'app-tab1',
@@ -9,76 +11,108 @@ import { Platform } from '@ionic/angular';
   styleUrls: ['tab1.page.scss'],
 })
 export class Tab1Page {
-  map: L.Map | undefined;
+  address: string = "";
+  map!: L.Map;
+  greenIcon: L.Icon = L.icon({
+    iconUrl: '../../assets/icon/zipfel.png',
+
+    iconSize: [50, 50], // size of the icon
+  });
   // langitude: number = 0;
   // altitude: number = 0;
-  constructor(protected platform: Platform, private geolocation: Geolocation) {          
-   }
+  constructor(protected platform: Platform, private geolocation: Geolocation) {
+  }
   ngOnInit() {
-    //Generate Start map   
-    this.map = L.map('map', {
-      center: [48.30694, 14.28583],
-      zoom: 12,
-      renderer: L.canvas(),
-    });
-
-    //Get Position of User
-    this.getGeoLocation();
-
     //Define Icon
-    var greenIcon = L.icon({
-      iconUrl: '../../assets/icon/zipfel.png',
 
-      iconSize: [50, 50], // size of the icon
-      // shadowSize: [50, 64], // size of the shadow
-      // iconAnchor: [22, 94], // point of the icon which will correspond to marker's location
-      // shadowAnchor: [4, 62],  // the same for the shadow
-      // popupAnchor: [-3, -76] // point from which the popup should open relative to the iconAnchor
+
+    //Generate Start map
+    this.geolocation.getCurrentPosition().then((resp) => {
+      this.map = L.map('map', {
+        center: [resp.coords.latitude, resp.coords.longitude],
+        zoom: 12,
+        renderer: L.canvas(),
+      });
+      L.circleMarker([resp.coords.latitude, resp.coords.longitude]).addTo(this.map);
+
+      L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '© Vegastro',
+      }).addTo(this.map);
+
+      setTimeout(() => {
+        this.map.invalidateSize();
+      }, 0);
+      this.addMarker(48.0349017, 13.7643518, this.greenIcon);
     });
+    //Get Position of User
+    //this.setMapToUserLocation();
 
-    //Inserts (Optional)
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      attribution: '© Vegastro',
-    }).addTo(this.map);
-
-    setTimeout(() => {
-      this.map?.invalidateSize();
-    }, 0);
-
-    //Place Marker on Map
-    L.marker([51.5, -0.09], { icon: greenIcon })
-      .addTo(this.map)
-      .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-      .openPopup();
-
-      
   }
 
-  
-
-  //Get Location of User
-  getGeoLocation() {
+  // //Get Location of User
+  setMapToUserLocation() {
     this.geolocation
       .getCurrentPosition()
       .then((resp) => {
-        //return [resp.coords.latitude, resp.coords.longitude]
-        console.log(resp.coords.latitude, resp.coords.longitude);
-        
-        this.map?.panTo(
-          new L.LatLng(resp.coords.latitude, resp.coords.longitude)
-        );
-
-        
+        this.map.panTo([resp.coords.latitude, resp.coords.longitude]);
+        L.circleMarker([resp.coords.latitude, resp.coords.longitude]).addTo(this.map)
       })
       .catch((error) => {
         console.log('Error getting location', error);
       });
-
-    let watch = this.geolocation.watchPosition();
-    watch.subscribe((data) => {
-      // data can be a set of coordinates, or an error (if an error occurred).
-      // data.coords.latitude
-      // data.coords.longitude
-    });
   }
+
+  addMarker(latitude: number, longitude: number, icon: L.Icon) {
+    L.marker([latitude, longitude], { icon: icon }).addTo(this.map);
+  }
+
+  addressSearch(enter: Boolean, index?: number) {
+    setTimeout(()=>{
+      this.addrSearch((data: any) => {
+        if(document.getElementById("searchbarResultList") != null) {
+          document.getElementById("searchbarResultList")?.remove();
+        }      
+        if (data.length > 0) {
+          if(enter) {
+            this.map.panTo([parseFloat(data[0].lat), parseFloat(data[0].lon)]);
+          } else if (index != undefined && index != null) {
+            this.map.panTo([parseFloat(data[index].lat),parseFloat(data[index].lon)]);
+          } else {
+            let list = document.createElement("ion-list");
+            for(let i = 0; i < data.length; i++) {
+              let item = document.createElement("ion-item");
+              let label = document.createElement("p");
+              let text = data[i].display_name;
+              if(text <= 30) {
+                label.innerText = text;
+              } else {
+                label.innerText = text.slice(0,30) + "...";
+              }
+              item.appendChild(label);
+              item.addEventListener('click', ()=>{this.addressSearch(true,i)});
+              list?.appendChild(item);
+            }
+            list.id = "searchbarResultList";
+            document.getElementById("searchbar")?.appendChild(list);
+          }
+        }
+      }, this.address);
+    }, 250);
+    
+  }
+
+  addrSearch(callback: Function, address: string) {
+    var xmlhttp = new XMLHttpRequest();
+    var url = "https://nominatim.openstreetmap.org/search?format=json&limit=3&q=" + address;
+    xmlhttp.onreadystatechange = function () {
+      if (this.readyState == 4 && this.status == 200) {
+        var arr = JSON.parse(this.responseText);
+        callback(arr);
+      }
+    };
+    xmlhttp.open("GET", url, true);
+    xmlhttp.send();
+  }
+
+
 }
