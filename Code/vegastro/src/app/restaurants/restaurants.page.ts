693b8a46682fd93594bee1f7909f6d9d79778746
a.zauner <alexanderzauner29@gmail.com>
2023-02-09 11:30:57 +0100

    no hits display bug fixed

--- Code/vegastro/src/app/restaurants/restaurants.page.ts
+++ Code/vegastro/src/app/restaurants/restaurants.page.ts
@@ -29,42 +29,46 @@ export class RestaurantsPage {
   }
 
   updateRestaurants() {
-    const bounds = MapPage.map.getBounds();
-    if (
-      bounds.getNorthEast().lat != this.currentNorthEastLat ||
-      bounds.getNorthEast().lng != this.currentNorthEastLon ||
-      bounds.getSouthWest().lat != this.currentSouthWestLat ||
-      bounds.getSouthWest().lng != this.currentSouthWestLon
-    ) {
-      let restaurantCardList = document.getElementById('restaurantCards')
-          restaurantCardList!.innerHTML = '';
-      this.currentNorthEastLat = bounds.getNorthEast().lat;
-      this.currentNorthEastLon = bounds.getNorthEast().lng;
-      this.currentSouthWestLat = bounds.getSouthWest().lat;
-      this.currentSouthWestLon = bounds.getSouthWest().lng;
-      axios
-        .get(
-          'http://localhost:3000/restaurant/getNearPosition/' +
-          bounds.getNorthEast().lat +
-          '/' +
-          bounds.getNorthEast().lng +
-          '/' +
-          bounds.getSouthWest().lat +
-          '/' +
-          bounds.getSouthWest().lng
-        )
-        .then((response) => {
-          
-          if (response.data.status != 404) {
-            document.getElementById('restaurantCards')!.innerHTML = "<h1 style='font-size: 4vh;margin: 3.5vh 16px 0 16px;text-align: center;'>T R E F F E R</h1>"
-            for (const restaurant of response.data) {
-              let desc = restaurant.description ? restaurant.description : "";
-              this.service.addDynamicComponent({ id: restaurant.id, image: "pizzaDemo.png", restaurantName: restaurant.restaurantName, description: desc, type: "meat", stars: Math.floor(Math.random() * 5 + 1), menu: restaurant.menu })
+    if(MapPage.map) {
+      const bounds = MapPage.map.getBounds();
+      if (
+        bounds.getNorthEast().lat != this.currentNorthEastLat ||
+        bounds.getNorthEast().lng != this.currentNorthEastLon ||
+        bounds.getSouthWest().lat != this.currentSouthWestLat ||
+        bounds.getSouthWest().lng != this.currentSouthWestLon
+      ) {
+        let restaurantCardList = document.getElementById('restaurantCards')
+            restaurantCardList!.innerHTML = '';
+        this.currentNorthEastLat = bounds.getNorthEast().lat;
+        this.currentNorthEastLon = bounds.getNorthEast().lng;
+        this.currentSouthWestLat = bounds.getSouthWest().lat;
+        this.currentSouthWestLon = bounds.getSouthWest().lng;
+        axios
+          .get(
+            'http://localhost:3000/restaurant/getNearPosition/' +
+            bounds.getNorthEast().lat +
+            '/' +
+            bounds.getNorthEast().lng +
+            '/' +
+            bounds.getSouthWest().lat +
+            '/' +
+            bounds.getSouthWest().lng
+          )
+          .then((response) => {
+            
+            if (response.data.status != 404) {
+              if(document.getElementById('restaurantCards')) {
+                document.getElementById('restaurantCards')!.innerHTML = "<h1 style='font-size: 4vh;margin: 3.5vh 16px 0 16px;text-align: center;'>T R E F F E R</h1>"
+              }
+              for (const restaurant of response.data) {
+                let desc = restaurant.description ? restaurant.description : "";
+                this.service.addDynamicComponent({ id: restaurant.id, image: "pizzaDemo.png", restaurantName: restaurant.restaurantName, description: desc, type: "meat", stars: Math.floor(Math.random() * 5 + 1), menu: restaurant.menu })
+              }
+            } else if (document.getElementById('restaurantCards')){
+              document.getElementById('restaurantCards')!.innerHTML = "<h1 style='margin-top: 75%; text-align: center; font-size: 3vh;'>Keine Treffer gefunden</h1>"
             }
-          } else {
-            document.getElementById('restaurantCards')!.innerHTML = "<h1 style='margin-top: 75%; text-align: center; font-size: 3vh;'>Keine Treffer gefunden</h1>"
-          }
-        });
+          });
+      }
     }
   }
 }
