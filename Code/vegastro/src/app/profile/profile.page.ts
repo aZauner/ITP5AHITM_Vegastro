b5a1c301315d99d603ae3d865c449a644d0cfaa4
selmecker <71625985+selmecker@users.noreply.github.com>
2023-03-02 16:37:56 +0100

    Profile page

--- Code/vegastro/src/app/profile/profile.page.ts
+++ Code/vegastro/src/app/profile/profile.page.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import axios from 'axios';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.page.html',
   styleUrls: ['./profile.page.scss'],
 })
+
+
+
 export class ProfilePage implements OnInit {
 
   constructor() { }
 
-  ngOnInit() {
+  ngOnInit(){
+
+  }
+  ngAfterViewInit() {
+    axios.get('http://localhost:3000/user/' + sessionStorage.getItem('userToken')).then((response) => {
+        console.log(response.data);
+        console.log( document.getElementById("nameInput"));
+        document.getElementById("nameInput");
+
+      })
   }
 
+  
+  
+   // inputs: ProfilePage = this.defaultInputs;
+  
+  
 }
