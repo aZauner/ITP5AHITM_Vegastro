cc153635021f0b49f6d2c7a7df1a9a9a190d1132
StadlerTobias <12.01tobias@gmail.com>
2023-03-14 12:34:13 +0100

    Added loggout and Toast

--- Code/vegastro/src/app/tabs/tabs.page.ts
+++ Code/vegastro/src/app/tabs/tabs.page.ts
@@ -3,6 +3,7 @@ import { FavouritesPage } from '../favourites/favourites.page';
 import { MapPage } from '../map/map.page';
 import { RestaurantCardService } from '../restaurantCard/RestaurantCardService';
 import { RestaurantsPage } from '../restaurants/restaurants.page';
+import { IonModal, ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tabs',
@@ -11,11 +12,36 @@ import { RestaurantsPage } from '../restaurants/restaurants.page';
 })
 export class TabsPage {
 
-  constructor(private service: RestaurantCardService) {}
+  loggedIn = false;
 
+  constructor(private service: RestaurantCardService,  private toastController: ToastController) {}
+  
   update() {
     MapPage.getFavRests();
     RestaurantsPage.updateRestaurantsStatic(this.service);
     FavouritesPage.updateRestaurantsStatic(this.service);
+    if(sessionStorage.getItem('userToken')){
+      this.loggedIn = true
+    }else{
+      this.loggedIn = false
+    }
+         
   }
+
+  logout(){
+    sessionStorage.clear() 
+    this.callToast(1300)   
+  }
+
+  async callToast(duration: number) {
+    const toast = await this.toastController.create({
+      message: 'Sie sind abgemeldet',
+      duration: duration,
+      position: 'middle',      
+      cssClass: 'LoggedOutToast'      
+    });
+    await toast.present();
+  }
+
+
 }
