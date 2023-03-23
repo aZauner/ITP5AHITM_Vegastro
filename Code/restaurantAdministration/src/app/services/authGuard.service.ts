9c56514069e12a4c6bdd5f6d9c8eee3ceac46f0d
StadlerTobias <12.01tobias@gmail.com>
2023-03-23 17:11:19 +0100

    Added login with validation

---
+++ Code/restaurantAdministration/src/app/services/authGuard.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { Router, CanActivate, ActivatedRouteSnapshot,RouterStateSnapshot } from '@angular/router';
+
+
+@Injectable()
+export class User implements CanActivate {
+
+  constructor(private _router:Router ) {
+  }
+
+  canActivate(route: ActivatedRouteSnapshot,
+              state: RouterStateSnapshot): boolean {
+
+    //check some condition
+    if (sessionStorage.getItem("userToken") == undefined)  {
+      this._router.navigateByUrl("/login")
+      //redirect to login/home page etc
+      //return false to cancel the navigation
+      return false;
+    }
+    return true;
+
+  }
+
+}
