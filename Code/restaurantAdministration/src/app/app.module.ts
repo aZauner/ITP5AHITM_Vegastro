782f056e367d901b9beb2b3bb1c5491fb1708c4f
StadlerTobias <12.01tobias@gmail.com>
2023-03-23 09:25:34 +0100

    Added Admin Project

---
+++ Code/restaurantAdministration/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginPageComponent } from './login-page/login-page.component';
+import {RouterModule, RouterOutlet, Routes} from "@angular/router";
+import { DashboardComponent } from './dashboard/dashboard.component';
+
+const appRoutes: Routes = [
+  // {path: '/' , component: AppComponent}
+]
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginPageComponent,
+    DashboardComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    RouterModule.forRoot(appRoutes)
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
