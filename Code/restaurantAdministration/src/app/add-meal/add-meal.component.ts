66d1217398a8eaa5d123e505388c6c85e3df6c11
StadlerTobias <12.01tobias@gmail.com>
2023-04-11 12:39:35 +0200

    added form to create meal

--- Code/restaurantAdministration/src/app/add-meal/add-meal.component.ts
+++ Code/restaurantAdministration/src/app/add-meal/add-meal.component.ts
@@ -1,5 +1,14 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { UserService } from '../services/user.service';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} 
+from '@angular/forms';
+import axios from 'axios';
 
 @Component({
   selector: 'app-add-meal',
@@ -16,5 +25,32 @@ export class AddMealComponent {
   ngOnInit(){
     this.id = this.route.snapshot.params['id'];
     console.log(this.id) 
+  }
+
+  formBuilder: FormBuilder = new FormBuilder
+
+  firstFormGroup = this.formBuilder.group({
+    mealName: ['', Validators.minLength(2)],
+    description: ['', Validators.minLength(2)],
+    type: ['', Validators.minLength(2)],
+    price: ['', Validators.minLength(2)],
+  });
+  
+
+  hide = true;
+  types = ['vegan', 'meat', 'vegetarian'];
+  location = { street: '', housenumber: '', plz: '', city: '', floor: '' };
+  createInputs = {
+    restaurantName: '',
+    latitude: 0,
+    longitude: 0,
+    owner: '',
+    type: '',
+    description: '',
+    location: { city: '', plz: '', street: '', housenumber: '', floor: '' },
+  };
+
+  create(){
+    console.log("test");    
   }
 }
