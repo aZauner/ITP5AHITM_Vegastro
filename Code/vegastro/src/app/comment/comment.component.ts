929ac2a58cb9d32b47ef21030b465208be00c847
StadlerTobias <71379758+StadlerTobias@users.noreply.github.com>
2023-04-28 10:32:46 +0200

    added modal to edit comment

--- Code/vegastro/src/app/comment/comment.component.ts
+++ Code/vegastro/src/app/comment/comment.component.ts
@@ -1,4 +1,6 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, ViewChild } from '@angular/core';
+import { IonModal, ModalController } from '@ionic/angular';
+
 
 @Component({
   selector: 'comment',
@@ -7,5 +9,28 @@ import { Component, Input } from '@angular/core';
 })
 
 export class Comment {
-  @Input() inputs = {comment: "",stars: 0}
+  @Input() inputs = {comment: "",stars: 0, userToken: ""}
+  @Input() id = 0
+
+  @ViewChild(IonModal)
+  modal!: IonModal;
+
+  isOwnComment = false
+  modalOpen = false
+
+  constructor(public modalController: ModalController){}
+
+  ngOnInit(){      
+    if(this.inputs.userToken == sessionStorage.getItem("userToken")){
+      this.isOwnComment = true; 
+    }
+  }
+
+  cancel() {
+    this.modal.dismiss(null, 'cancel');
+  }
+
+  confirm(){
+    console.log("vvcx");    
+  }  
 }
