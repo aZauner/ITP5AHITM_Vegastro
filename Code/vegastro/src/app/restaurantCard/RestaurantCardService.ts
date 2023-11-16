db41df4f61e9a8d9817552330e68c6e6cb38ba07
a.zauner <alexanderzauner29@gmail.com>
2023-11-16 20:24:22 +0100

    closes 32;

--- Code/vegastro/src/app/restaurantCard/RestaurantCardService.ts
+++ Code/vegastro/src/app/restaurantCard/RestaurantCardService.ts
@@ -18,21 +18,21 @@ export class RestaurantCardService {
     private resolver: ComponentFactoryResolver,
     private injector: Injector,
     private app: ApplicationRef
-  ) {}
+  ) { }
 
   async addDynamicComponent(inputs: RestaurantCardInputs) {
-    await this.getAverageStarts(inputs.id).then((starRating) => {      
-           
-    inputs.stars =  Math.round(starRating * 100) / 100
-    this.mealDivision(inputs); 
-      inputs.mealDevisionInputs = this.mealDevisionInputs; 
-      
-      
+    await this.getAverageStarts(inputs.id).then((starRating) => {
+
+      inputs.stars = Math.round(starRating * 100) / 100
+      this.mealDivision(inputs);
+      inputs.mealDevisionInputs = this.mealDevisionInputs;
+
+
       const factory = this.resolver.resolveComponentFactory(RestaurantCard);
       const div = document.createElement('div');
       document.getElementById('restaurantCards')!.appendChild(div);
       const component = factory.create(this.injector, [], div);
-      
+
       switch (inputs.type) {
         case 'meat':
           inputs.type = 'MeatIcon.svg';
@@ -44,7 +44,7 @@ export class RestaurantCardService {
           inputs.type = 'VeganIcon.svg';
           break;
       }
-      
+
       if (inputs.description.length > 150) {
         inputs.preDescr = inputs.description.slice(0, 150) + ' ...';
       } else {
@@ -58,8 +58,8 @@ export class RestaurantCardService {
   async addDynamicComponentFav(inputs: RestaurantCardInputs) {
     await this.getAverageStarts(inputs.id).then((starRating) => {
       inputs.stars = starRating;
-      this.mealDivision(inputs); 
-      inputs.mealDevisionInputs = this.mealDevisionInputs;      
+      this.mealDivision(inputs);
+      inputs.mealDevisionInputs = this.mealDevisionInputs;
 
       const factory = this.resolver.resolveComponentFactory(RestaurantCard);
       const div = document.createElement('div');
@@ -80,17 +80,17 @@ export class RestaurantCardService {
         inputs.preDescr = inputs.description.slice(0, 150) + ' ...';
       } else {
         inputs.preDescr = inputs.description;
-      }           
+      }
       component.instance.inputs = inputs;
-      this.app.attachView(component.hostView);      
+      this.app.attachView(component.hostView);
     });
   }
-  
+
 
   async getAverageStarts(id: string) {
     let ratingstars = 0;
     await axios
-      .get(BASE_URL+'/rating/byRestaurant/' + id)
+      .get(BASE_URL + '/rating/byRestaurant/' + id)
       .then((response) => {
         let sumStars = 0;
         if (response.data.length > 0) {
@@ -98,7 +98,7 @@ export class RestaurantCardService {
             sumStars += star.stars;
           }
           ratingstars = sumStars / response.data.length;
-        }        
+        }
       });
     return ratingstars;
   }
@@ -110,7 +110,7 @@ export class RestaurantCardService {
       vegan: 0,
       vegetarian: 0
     }
-    
+
     if (inputs.menu.length > 0) {
       for (let i = 0; i < inputs.menu.length; i++) {
         if (inputs.menu[i].type == 'other') {
@@ -126,10 +126,9 @@ export class RestaurantCardService {
       devisionPerMeal.other = devisionPerMeal.other / inputs.menu.length;
       devisionPerMeal.vegetarian = devisionPerMeal.vegetarian / inputs.menu.length
       devisionPerMeal.vegan = devisionPerMeal.vegan / inputs.menu.length
-      
+
     }
     this.mealDevisionInputs = devisionPerMeal;
-    console.log( this.mealDevisionInputs);
-    
+
   }
 }
