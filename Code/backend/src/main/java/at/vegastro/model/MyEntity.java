6e4764768aa0003f78a375275e68953b56ebdd99
StadlerTobias <71379758+StadlerTobias@users.noreply.github.com>
2023-11-17 11:19:49 +0100

    file strukture

--- Code/backend/src/main/java/at/vegastro/model/MyEntity.java
+++ Code/backend/src/main/java/at/vegastro/model/MyEntity.java
@@ -1,4 +1,4 @@
-package at.vegastro;
+package at.vegastro.model;
 
 import io.quarkus.hibernate.orm.panache.PanacheEntity;
 import jakarta.persistence.Entity;
--- Code/backend/src/main/java/at/vegastro/resource/ExampleResource.java
+++ Code/backend/src/main/java/at/vegastro/resource/ExampleResource.java
@@ -1,4 +1,4 @@
-package at.vegastro;
+package at.vegastro.resource;
 
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
