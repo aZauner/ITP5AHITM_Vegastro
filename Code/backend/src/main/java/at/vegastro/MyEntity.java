ccf76132bb4ec7d55d27a18445353c092ae3199d
StadlerTobias <71379758+StadlerTobias@users.noreply.github.com>
2023-11-17 11:11:02 +0100

    added quarkus backend initial state

---
+++ Code/backend/src/main/java/at/vegastro/MyEntity.java
@@ -0,0 +1,10 @@
+package at.vegastro;
+
+import io.quarkus.hibernate.orm.panache.PanacheEntity;
+import jakarta.persistence.Entity;
+
+
+@Entity
+public class MyEntity extends PanacheEntity {
+    public String field;
+}
