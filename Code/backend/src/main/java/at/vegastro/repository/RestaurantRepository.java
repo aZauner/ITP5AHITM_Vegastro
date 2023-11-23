db26e27e09afd13dfe86ebd2ee411733e5258aba
a.zauner <alexanderzauner29@gmail.com>
2023-11-23 11:50:28 +0100

    find by location

--- Code/backend/src/main/java/at/vegastro/repository/RestaurantRepository.java
+++ Code/backend/src/main/java/at/vegastro/repository/RestaurantRepository.java
@@ -6,6 +6,7 @@ import at.vegastro.model.Restaurant;
 import io.quarkus.hibernate.orm.panache.PanacheRepository;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
+import jakarta.persistence.TypedQuery;
 
 import java.util.HashMap;
 import java.util.List;
@@ -28,11 +29,13 @@ public class RestaurantRepository implements PanacheRepository<Restaurant> {
     }
 
     public List<Restaurant> findRestaurantsNearPosition(Double northLat, Double northLon, Double southLat, Double southLon) {
-        Map<String,Double> params = new HashMap<>();
-        params.put("northLat", northLat);
-        params.put("northLon", northLon);
-        params.put("southLat", southLat);
-        params.put("southLon", southLon);
-        return list("longitude < :northLon and longitude > :southLon and latitude < :northLat and latitude > :southLat", params);
+        TypedQuery<Restaurant> query = getEntityManager().createQuery(
+                "select r from Restaurant r where r.latitude < :northLat and r.latitude > :southLat and r.longitude < :northLon and r.longitude > : southLon", Restaurant.class
+        );
+        query.setParameter("northLat", northLat);
+        query.setParameter("northLon", northLon);
+        query.setParameter("southLat", southLat);
+        query.setParameter("southLon", southLon);
+        return query.getResultList();
     }
 }
