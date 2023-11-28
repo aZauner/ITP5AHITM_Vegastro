100915f6af6f092c7e6e0823dbe33dc545654dc2
a.zauner <alexanderzauner29@gmail.com>
2023-11-28 10:54:04 +0100

    import sql

--- Code/backend/src/main/resources/import.sql
+++ Code/backend/src/main/resources/import.sql
@@ -5,4 +5,13 @@
 -- insert into myentity (id, field) values(3, 'field-3');
 -- alter sequence myentity_seq restart with 4;
 
+insert into Users(id,firstname, lastname, username, email, password) values (1,'Alexander', 'Zauner', 'zauner', 'vegastro@gmail.com', '307fd56085a6ed891cfb2c38276d7853b122968754378f1153e0127f3a764a0f');
+-- password 12342
 
+insert into location(id,city, plz, street, housenumber, floor) values (1,'Redlham', 4846, 'Au', 77, 4);
+
+insert into Restaurant(id,restaurantName,latitude,longitude,owner_id,type,description,location_id) values (1,'Food Palace', 48.2652888, 14.2453703, 1, 'meat', 'Test Description', 1);
+
+insert into Rating(id,user_id,restaurant_id,stars,comment) values(1,1,1,5,'Gutes Essen');
+
+insert into ratingupvotes(id,user_id,rating_id) values(1,1,1);
